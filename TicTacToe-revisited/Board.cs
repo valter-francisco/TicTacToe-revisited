eca3e8ef1385cec3e2a8795169364e45c4996441
ValterFrancisco <ValterFrancisco@PT-AL-TEC005>
2022-02-14 20:45:39 +0000

    Add project files.

---
+++ TicTacToe-revisited/Board.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace TicTacToe_revisited
+{
+    public class Board
+    {
+        public int Line;
+        public int Collumn;
+
+        private string[,] board;// = new string[5, 5];
+
+        public static void DrawBoard (string[,] board, int line, int collumn)
+        {
+            board = new string[5, 5];
+
+            for (line = 0; line < 5; line++)
+            {
+                for (collumn = 0; collumn < 5; collumn++)
+                {
+                    if (line % 2 != 0)
+                    {
+                        board[line,collumn] = "---";
+                    }
+                    else if (collumn % 2 != 0)
+                    {
+                        board[line, collumn] = " | ";
+                    }
+                    Console.Write(board[line,collumn]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ TicTacToe-revisited/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace TicTacToe_revisited
+{
+    public class Program
+    {
+
+        Board board1 = new Board();
+
+        Board board2 = new Board();
+       
+        
+
+
+
+    }
+}
